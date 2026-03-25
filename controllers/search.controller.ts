9ddfebad072ec053aa1758ae058de37cac900fcf
terminalwarlord <joybiswas389@gmail.com>
2026-03-26 01:36:07 +0600

    feat(backend): add Redis caching on search controller

--- controllers/search.controller.ts
+++ controllers/search.controller.ts
@@ -2,10 +2,12 @@ import type { Context } from "hono";
 import z from "zod";
 import { meilisearch_client } from "../lib/meilisearch/meilisearch_client";
 import { signDownloadUrl } from "../lib/r2/sign_download_image";
-import { SortBy } from "../types/product_type";
+import { SortBy, type Product } from "../types/product_type";
+import { redis_client } from "../lib/redis/redis_client";
+import { generateHash } from "../lib/utils/hash_key";
 
 export const getSearchController = async (c: Context) => {
-	// TODO: add rate limits and caching
+	// TODO: add rate limits
 	const schema = z.object({
 		limit: z.coerce.number().min(1).max(20).default(20),
 		offset: z.coerce.number().min(0).default(0),
@@ -30,12 +32,30 @@ export const getSearchController = async (c: Context) => {
 		);
 	}
 	const { offset, limit, sortBy, sortOrder, query } = parsedData.data;
+	// Check for redis hit
+	const cache_key =
+		"pricelens_search:" +
+		generateHash(`${query}:${limit}:${offset}:${sortBy}:${sortOrder}`);
+	const redisHit = await redis_client.get(cache_key);
+	if (redisHit) {
+		console.info(`Cache hit!`);
+		const data = JSON.parse(redisHit) as Product[];
+		return c.json(
+			data.map((item) => {
+				const productImage = signDownloadUrl(item.product_image);
+				return {
+					...item,
+					product_image: productImage,
+				};
+			}),
+		);
+	}
 	const results = await meilisearch_client.index("products").search(query, {
 		limit,
 		offset,
 		sort: [`${sortBy}:${sortOrder}`],
 	});
-
+	await redis_client.set(cache_key, JSON.stringify(results.hits), "EX", 300);
 	const hits = await Promise.all(
 		results.hits.map((item) => {
 			const productImage = signDownloadUrl(item.product_image);
