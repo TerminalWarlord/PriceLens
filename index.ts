ed2635c7081734b570ca70fdd67f5a600b2b9601
terminalwarlord <joybiswas389@gmail.com>
2026-03-27 23:05:57 +0600

    feat(backend): apply CORS and rate limit middleware to search endpoint

--- index.ts
+++ index.ts
@@ -1,15 +1,21 @@
 import { config } from "dotenv";
 import { Hono } from "hono";
 import { getSearchController } from "./controllers/search.controller";
+import { cors } from "hono/cors";
+import { rateLimiter } from "./middlewares/rate_limiter";
 
 config();
 
 const app = new Hono();
 
-// TODO: add cors, rate limiter
+app.use(
+	cors({
+		origin: ["http://localhost:5173", "https://pricelens.joybiswas.com"],
+	}),
+);
 
-app.get("/search", getSearchController);
-app.get("/", (c) => c.text("Hono!"));
+app.get("/", rateLimiter, (c) => c.text("Hello!"));
+app.get("/search", rateLimiter, getSearchController);
 
 Bun.serve({
 	port: process.env.PORT,
