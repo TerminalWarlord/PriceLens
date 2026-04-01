0ac360521f69ec69d8f9bb6577f29fa6da784688
terminalwarlord <joybiswas389@gmail.com>
2026-04-02 00:33:32 +0600

    chore(backend): change default sort order to be RELEVANCE

--- controllers/search.controller.ts
+++ controllers/search.controller.ts
@@ -11,7 +11,7 @@ export const getSearchController = async (c: Context) => {
 		limit: z.coerce.number().min(1).max(20).default(20),
 		offset: z.coerce.number().min(0).default(0),
 		query: z.string().optional(),
-		sortBy: z.enum(SortBy).default(SortBy.PRODUCT_PRICE),
+		sortBy: z.enum(SortBy).default(SortBy.RELEVANCE),
 		sortOrder: z.enum(["ASC", "DESC"]).default("ASC"),
 		minPrice: z.coerce.number().optional(),
 		maxPrice: z.coerce.number().optional(),
