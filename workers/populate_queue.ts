56cd21290e1fdaba8c7a9a8d70637b9d7a890d2a
terminalwarlord <joybiswas389@gmail.com>
2026-04-08 20:31:53 +0600

    chore(workers): remove Computer Village from populate_queue

--- workers/populate_queue.ts
+++ workers/populate_queue.ts
@@ -1,18 +1,15 @@
 import { scrapeAppleGadgetsCategories } from "../lib/scrapers/apple_gadgets";
-import { scrapeComputerVillageCategories } from "../lib/scrapers/computer_village";
 import { scrapeStartechCategories } from "../lib/scrapers/startech";
 import { scrapeTechMarvelsCategories } from "../lib/scrapers/tech_marvels";
 import { scrapeTechlandCategories } from "../lib/scrapers/techland";
 import { scrapeUCCCategories } from "../lib/scrapers/ucc";
 import { redis_client } from "../lib/redis/redis_client";
 import { flushQueueAndSet, setTtlOnQueue } from "../lib/redis/redis_helper";
-import { processQueue } from "./process_queue";
 
 async function populateQueue() {
 	await flushQueueAndSet();
 	await scrapeStartechCategories();
 	await scrapeTechlandCategories();
-	await scrapeComputerVillageCategories();
 	await scrapeUCCCategories();
 	await scrapeAppleGadgetsCategories();
 	await scrapeTechMarvelsCategories();
